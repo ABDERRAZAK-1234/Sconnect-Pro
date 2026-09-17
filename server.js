6186f5662deab8f0d423d2efa649f3e28f6b8858
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-17 16:14:15 +0100

    J'ai fait des modifications dans le server.js

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
+require('dotenv').config();
+
 const http = require('http');
-const db = require('./src/config/db');
-const { json } = require('stream/consumers');
+const router = require('./src/core/router');
+
 const port = process.env.PORT_Node || 3000;
-const server = http.createServer(async (req, res) => {
-    try {
-        const result = await db.query("SELECT NOW()");
 
-        res.writeHead(200, {
-            "content-type": "application/json"
-        });
-
-        res.end(JSON.stringify({
-            message: "SportConnect is running ...",
-            database: "Connected",
-            time: result.rows[0].now
-        }));
-
-
-    } catch (error) {
-        console.log("Erreur", error);
-
-        res.writeHead(500, {
-            "Content-Type": "application/json"
-        });
-
-        res.end(JSON.stringify({
-            error: "Erreur de connexion à PostgreSQL"
-        }));
-    }
+const server = http.createServer((req, res) => {
+    router.lookup(req, res);
 });
 
-
 server.listen(port, () => {
-    console.log(`Le serveur est demarré sur le port :${port}`);
+    console.log(`Le serveur est demarré sur le port : ${port}`);
 });
