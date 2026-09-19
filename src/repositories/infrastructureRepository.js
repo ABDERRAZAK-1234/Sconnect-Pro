797e9ed886cf0b3a635ee763483d4e4fe77f6212
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-19 19:57:19 +0100

    J'ai ajouter du CRUD (create, update, remove) pour infrastructure

--- src/repositories/infrastructureRepository.js
+++ src/repositories/infrastructureRepository.js
@@ -8,7 +8,22 @@ const findById = async (id) => {
     return await baseRepository.findById("infrastructure", id);
 };
 
+const create = (data) => {
+    return baseRepository.create("infrastructure", data);
+};
+
+const update = (id, data) => {
+    return baseRepository.update("infrastructure", id, data);
+};
+
+const remove = (id) => {
+    return baseRepository.remove("infrastructure", id);
+};
+
 module.exports = {
     findAll,
-    findById
+    findById,
+    create,
+    update,
+    remove
 };
