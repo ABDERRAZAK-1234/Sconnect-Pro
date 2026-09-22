2bafbf9fd8014b0cc5bfd92cadf0270c77c867bf
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-22 14:08:34 +0000

    J'ai ajouté countFamilyRegistrations dans le repository inscription pour compter les inscriptions actives d'une famille

--- src/repositories/inscriptionRepository.js
+++ src/repositories/inscriptionRepository.js
@@ -20,10 +20,30 @@ const remove = (id) => {
     return baseRepository.remove("inscription", id);
 };
 
+const countFamilyRegistrations = async (familleId) => {
+    const db = require("../config/db");
+
+    const result = await db.query(
+        `SELECT COUNT(*) AS total
+         FROM inscription i
+         JOIN membre m ON m.id = i.membre_id
+         WHERE m.famille_id = $1
+         AND i.statut IN (
+             'confirmee',
+             'en_attente',
+             'promoted_pending'
+         )`,
+        [familleId]
+    );
+
+    return Number(result.rows[0].total);
+};
+
 module.exports = {
     findAll,
     findById,
     create,
     update,
-    remove
+    remove,
+    countFamilyRegistrations
 };
