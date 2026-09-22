09833b12fb38ae7b336a1f1bf04aee3e9905755b
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-09-22 21:52:01 +0000

    J'ai ajouté getAgeCategory pour déterminer la catégorie d'âge d'un membre

--- src/services/eligibilityService.js
+++ src/services/eligibilityService.js
@@ -23,6 +23,40 @@ const getAgeAtEndOfYear = (dateNaissance) => {
     return age;
 };
 
+const getAgeCategory = (age) => {
+
+    if (age <= 6) {
+        return "Eveil/Baby-Sport";
+    }
+
+    if (age >= 7 && age <= 8) {
+        return "Poussin U9";
+    }
+
+    if (age >= 9 && age <= 10) {
+        return "Benjamin U11";
+    }
+
+    if (age >= 11 && age <= 12) {
+        return "Minime U13";
+    }
+
+    if (age >= 13 && age <= 14) {
+        return "Cadet U15";
+    }
+
+    if (age >= 15 && age <= 17) {
+        return "Junior U18";
+    }
+
+    if (age >= 18 && age <= 39) {
+        return "Senior";
+    }
+
+    return "Veteran/Master";
+};
+
 module.exports = {
-    getAgeAtEndOfYear
+    getAgeAtEndOfYear,
+    getAgeCategory
 };
